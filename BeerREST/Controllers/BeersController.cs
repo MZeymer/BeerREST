86e1dd3a4ba66d73cd88f61ef7896bbb214f0217
MZeymer <62883265+MZeymer@users.noreply.github.com>
2021-03-12 12:32:48 +0100

    Add project files.

---
+++ BeerREST/Controllers/BeersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Beer_Testing;
+using BeerREST.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BeerREST.Controllers
+{
+    [Route("beers")]
+    [ApiController]
+    public class BeersController : ControllerBase
+    {
+
+        private readonly BeerManager _manager = new BeerManager();
+
+        // GET: api/<BeersController>
+        [HttpGet]
+        public IEnumerable<Beer> Get([FromQuery] string name, [FromQuery] string sortBy)
+        {
+            return _manager.GetAll(name, sortBy);
+        }
+
+        // GET api/<BeersController>/5
+        [HttpGet("{id}")]
+        public Beer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<BeersController>
+        [HttpPost]
+        public Beer Post([FromBody] Beer value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<BeersController>/5
+        [HttpPut("{id}")]
+        public Beer Put(int id, [FromBody] Beer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<BeersController>/5
+        [HttpDelete("{id}")]
+        public Beer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ BeerREST/Managers/BeerManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using Beer_Testing;
+
+namespace BeerREST.Managers
+{
+    public class BeerManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Beer> Data = new List<Beer>
+        {
+            new Beer(_nextId++, "Carlsberg", 25, 4.7),
+            new Beer(_nextId++, "Tuborg", 20, 5.2),
+            new Beer(_nextId++,"Grimbergen", 30, 5.7)
+            
+
+
+        };
+
+        public List<Beer> GetAll(string name = null, string sortBy = null)
+        {
+            List<Beer> beers = new List<Beer>(Data);
+
+            if (name != null)
+            {
+                beers = beers.FindAll(beer => beer.Name != null && beer.Name.StartsWith(name));
+            }
+
+            if (sortBy != null)
+            {
+                switch (sortBy.ToLower())
+                {
+                    case "name":
+                        beers = beers.OrderBy(beer => beer.Name).ToList();
+                        break;
+                    case "price":
+                        beers = beers.OrderBy(beer => beer.Price).ToList();
+                        break;
+                    case "abv":
+                        beers = beers.OrderBy(beer => beer.Abv).ToList();
+                        break;
+                }
+            }
+            return beers;
+        }
+
+        public Beer GetById(int id)
+        {
+            return Data.Find(beer => beer.Id == id);
+        }
+
+        public Beer Add(Beer newBeer)
+        {
+            newBeer.Id = _nextId++;
+            Data.Add(newBeer);
+            return newBeer;
+        }
+
+        public Beer Delete(int id)
+        {
+            Beer beer = Data.Find(beer1 => beer1.Id == id);
+            if (beer == null) return null;
+            Data.Remove(beer);
+            return beer;
+        }
+
+        public Beer Update(int id, Beer updatedBeer)
+        {
+            Beer beer = Data.Find(beer1 => beer1.Id == id);
+            if (beer == null) return null;
+            beer.Name = updatedBeer.Name;
+            beer.Price = updatedBeer.Price;
+            beer.Abv = updatedBeer.Abv;
+            return beer;
+        }
+    }
+}
